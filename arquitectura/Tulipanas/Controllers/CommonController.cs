dc9dca2e785eb091f58b144697e93d721a2c8d9d
carlosmbrenner <carlosmbrenner@gmail.com>
2022-10-22 18:48:36 -0500

    update publish settings

--- arquitectura/Tulipanas/Controllers/CommonController.cs
+++ arquitectura/Tulipanas/Controllers/CommonController.cs
@@ -6,18 +6,6 @@ namespace Tulipanas.Controllers
     [ApiController]
     public class CommonController : ControllerBase
     {
-       /* [HttpPost, Route("api/[controller]/Login")]
-        public IActionResult Login(Usuario usuarioIn)
-        {
-            UsuarioLogin usuario = AccessRepository.Login(usuarioIn.Prefijo, usuarioIn.NombreUsuario, Tools.Encrypt(usuarioIn.Password));
-            HttpContext.Session.SetInt32("IdUsuario", (int)usuario.IdUsuario);
-            HttpContext.Session.SetInt32("IdEmpresa", (int)usuario.IdEmpresa);
-            HttpContext.Session.SetString("MapAcciones", usuario.MapAcciones);
-            usuario.Modulos = AccessRepository.ObtenerMenuUsuario(usuario.IdUsuario);
-          
-        }*/
-
-
         [HttpPost, Route("api/[controller]/UploadFile")]
         public void UploadFile(IFormFile file)
         {
